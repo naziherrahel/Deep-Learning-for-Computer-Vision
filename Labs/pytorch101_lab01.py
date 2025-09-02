c60a7d31fcae6c2dbbf0ca98c085046d5a18718b
naziherrahel <naziherrahel@gmail.com>
2025-09-02 10:56:44 +0800

    lab01_task2

--- Labs/pytorch101_lab01.py
+++ Labs/pytorch101_lab01.py
@@ -32,3 +32,65 @@ def create_sample_tensor() -> Tensor:
     #                            END OF YOUR CODE                             #
     ###########################################################################
     return x
+
+def mutate_tensor(
+    x: Tensor, indices: List[Tuple[int, int]], values: List[float]) -> Tensor:
+    """
+    Mutate the tensor x according to indices and values. Specifically, indices
+    is a list [(i0, j0), (i1, j1), ... ] of integer indices, and values is a
+    list [v0, v1, ...] of values. This function should mutate x by setting:
+
+    x[i0, j0] = v0
+    x[i1, j1] = v1
+
+    and so on.
+
+    If the same index pair appears multiple times in indices, you should set x
+    to the last one.
+
+    Args:
+        x: A Tensor of shape (H, W)
+        indices: A list of N tuples [(i0, j0), (i1, j1), ..., ]
+        values: A list of N values [v0, v1, ...]
+
+    Returns:
+        The input tensor x
+    """
+    ##########################################################################
+    #                     TODO: Implement this function                      #
+    ##########################################################################
+    # Replace "pass" statement with your code
+    pass
+    ##########################################################################
+    #                            END OF YOUR CODE                            #
+    ##########################################################################
+    return x
+
+
+def count_tensor_elements(x: Tensor) -> int:
+    """
+    Count the number of scalar elements in a tensor x.
+
+    For example, a tensor of shape (10,) has 10 elements; a tensor of shape
+    (3, 4) has 12 elements; a tensor of shape (2, 3, 4) has 24 elements, etc.
+
+    You may not use the functions torch.numel or x.numel. The input tensor
+    should not be modified.
+
+    Args:
+        x: A tensor of any shape
+
+    Returns:
+        num_elements: An integer giving the number of scalar elements in x
+    """
+    num_elements = None
+    ##########################################################################
+    #                      TODO: Implement this function                     #
+    #   You CANNOT use the built-in functions torch.numel(x) or x.numel().   #
+    ##########################################################################
+    # Replace "pass" statement with your code
+    pass
+    ##########################################################################
+    #                            END OF YOUR CODE                            #
+    ##########################################################################
+    return num_elements
